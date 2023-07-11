99179faca91a6d656252e44fa144ff2603e7246f
NicoBrites <nicolas.britesv@gmail.com>
2023-07-11 00:31:27 -0300

    Ultimas identaciones

--- funciones.py
+++ funciones.py
@@ -10,11 +10,11 @@ from boss_final import Boss
 class Funciones:
 
     def crear_player(lista_json: list):
-            for objetos in lista_json:
-                for clave, valor in objetos.items():
-                    if clave == "player":
-                        return Player(x=valor["x"], y=valor["y"], speed_walk=valor["speed_walk"], speed_run=valor["speed_run"], gravity=valor["gravity"], jump_power=valor["jump_power"],
-                                    frame_rate_ms=valor["frame_rate_ms"], move_rate_ms=valor["move_rate_ms"], jump_heigh=valor["jump_heigh"], tipe=valor["tipe"])
+        for objetos in lista_json:
+            for clave, valor in objetos.items():
+                if clave == "player":
+                    return Player(x=valor["x"], y=valor["y"], speed_walk=valor["speed_walk"], speed_run=valor["speed_run"], gravity=valor["gravity"], jump_power=valor["jump_power"],
+                                  frame_rate_ms=valor["frame_rate_ms"], move_rate_ms=valor["move_rate_ms"], jump_heigh=valor["jump_heigh"], tipe=valor["tipe"])
 
     def crear_plataformas(lista_json: list):
         lista_plataformas = []
@@ -23,11 +23,11 @@ class Funciones:
                 if clave == "plataformas":
                     for plataformas in valor:
                         if plataformas["plataforma"] == "Piso_lvl2":
-                                lista_plataformas.append(Piso_lvl2(
-                                    x=plataformas["x"], y=plataformas["y"], w=plataformas["w"], h=plataformas["h"], type=plataformas["type"]))
+                            lista_plataformas.append(Piso_lvl2(
+                                x=plataformas["x"], y=plataformas["y"], w=plataformas["w"], h=plataformas["h"], type=plataformas["type"]))
                         elif plataformas["plataforma"] == "Plataforma_lvl2":
-                                lista_plataformas.append(Plataforma_lvl2(
-                                    x=plataformas["x"], y=plataformas["y"], w=plataformas["w"], h=plataformas["h"], type=plataformas["type"]))
+                            lista_plataformas.append(Plataforma_lvl2(
+                                x=plataformas["x"], y=plataformas["y"], w=plataformas["w"], h=plataformas["h"], type=plataformas["type"]))
                         elif plataformas["plataforma"] == "Plataforma_lvl2_dinamica":
                                 lista_plataformas.append(Plataforma_lvl2_dinamica(
                                     x=plataformas["x"], y=plataformas["y"], w=plataformas["w"], h=plataformas["h"], type=plataformas["type"]))
@@ -50,13 +50,13 @@ class Funciones:
                     for enemigos in valor:
                         if enemigos["enemigo"] == "Enemigo_drag_lvl2":
                             lista_enemigos.append(Enemigo_drag_lvl2(x=enemigos["x"], y=enemigos["y"],
-                                                                            frame_rate_ms=enemigos["frame_rate_ms"], move_rate_ms=enemigos["move_rate_ms"], type=enemigos["type"]))
+                                                                    frame_rate_ms=enemigos["frame_rate_ms"], move_rate_ms=enemigos["move_rate_ms"], type=enemigos["type"]))
                         elif enemigos["enemigo"] == "Enemigo":
                             lista_enemigos.append(Enemigo(x=enemigos["x"], y=enemigos["y"], speed_walk=enemigos["speed_walk"], speed_run=enemigos["speed_run"], gravity=enemigos["gravity"],
-                                                            frame_rate_ms=enemigos["frame_rate_ms"], move_rate_ms=enemigos["move_rate_ms"]))
+                                                          frame_rate_ms=enemigos["frame_rate_ms"], move_rate_ms=enemigos["move_rate_ms"]))
                         else:
                             lista_enemigos.append(Enemigo_calaca_lvl2(x=enemigos["x"], y=enemigos["y"], speed_walk=enemigos["speed_walk"], speed_run=enemigos["speed_run"], gravity=enemigos["gravity"],
-                                                                            frame_rate_ms=enemigos["frame_rate_ms"], move_rate_ms=enemigos["move_rate_ms"], type=enemigos["type"]))
+                                                                      frame_rate_ms=enemigos["frame_rate_ms"], move_rate_ms=enemigos["move_rate_ms"], type=enemigos["type"]))
         return lista_enemigos
 
     def crear_botines(lista_json: list):
@@ -73,16 +73,16 @@ class Funciones:
                                 Comidita(x=botines["x"], y=botines["y"], w=botines["w"], h=botines["h"]))
                         else:
                             lista_botinex.append(
-                                Console(x=botines["x"], y=botines["y"], w=botines["w"], h=botines["h"])) 
+                                Console(x=botines["x"], y=botines["y"], w=botines["w"], h=botines["h"]))
         return lista_botinex
 
     def crear_boss(lista_json: list):
-            for objetos in lista_json:
-                for clave, valor in objetos.items():
-                    if clave == "boss":
-                        return Boss(x=valor["x"], y=valor["y"], speed_walk=valor["speed_walk"], speed_run=valor["speed_run"], gravity=valor["gravity"],
-                                    frame_rate_ms=valor["frame_rate_ms"], move_rate_ms=valor["move_rate_ms"])
-         
+        for objetos in lista_json:
+            for clave, valor in objetos.items():
+                if clave == "boss":
+                    return Boss(x=valor["x"], y=valor["y"], speed_walk=valor["speed_walk"], speed_run=valor["speed_run"], gravity=valor["gravity"],
+                                frame_rate_ms=valor["frame_rate_ms"], move_rate_ms=valor["move_rate_ms"])
+
     def mute():
         pygame.mixer.music.set_volume(0)
 
